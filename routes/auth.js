eda1ddab942067fddc5ec517079b1c56ad66251b
John Su <30964725+JSN190@users.noreply.github.com>
2018-09-08 08:14:39 +0100

    Add POST /login method

--- routes/auth.js
+++ routes/auth.js
@@ -6,6 +6,51 @@ const check = require("express-validator/check");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
+router.post("/login", [
+        check.body("username").exists().isAlphanumeric().isLength({ max: 30 }),
+        check.body("password").exists().isAscii().isLength({ min: 8, max: 72 })
+    ], async (req, res) => {
+        res.type("application/json");
+        const errors = check.validationResult(req);
+        if (errors.isEmpty()) {
+            try {
+                const user = await database.query({
+                    text: "SELECT * FROM users WHERE username=$1",
+                    values: [req.body.username.toLowerCase()]
+                });
+                if (user.rows.length !== 1) {
+                    res.status(404);
+                    res.send({ error: true, details: `Username ${req.body.username} not found.`});
+                    return;
+                }
+                const correctPassword = await bcrypt.compare(req.body.password, user.rows[0].pass_hash);
+                if (correctPassword) {
+                    res.send({
+                        success: true,
+                        operation: "login",
+                        user: {
+                            id: user.rows[0].id,
+                            username: user.rows[0].display
+                        },
+                        token: jwt.sign({ id: user.rows[0].id, username: user.rows[0].display }, 
+                            process.env.LIGHTPOLL_JWT)
+                    });
+                } else {
+                    res.status(403);
+                    res.send({ error: true, details: `Incorrect password.`});
+                    return;
+                }
+            } catch (e) {
+                console.log(e);
+                res.status(500);
+                res.send({ error: true })
+            }
+        } else {
+            res.status(400);
+            res.send({ error: true, details: errors.array() });
+        }
+});
+
 router.post("/register", [
         check.body("username").exists().isAlphanumeric().isLength({ max: 30 }),
         check.body("password").exists().isAscii().isLength({ min: 8, max: 72 })
@@ -15,6 +60,7 @@ router.post("/register", [
         if (errors.isEmpty()) {
             const client = await database.connect();
             try {
+                await client.query("BEGIN");
                 const exists = (await client.query({
                     text: "SELECT * FROM users WHERE username=$1",
                     values: [req.body.username.toLowerCase()]
@@ -42,6 +88,7 @@ router.post("/register", [
                 });
             } catch (e) {
                 console.log(e);
+                await client.query("ROLLBACK");
                 res.status(500);
                 res.send({ error: true })
             } finally {
