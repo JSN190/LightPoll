b68949e98f90774085b509e4d26a6887e49f3e9b
John Su <30964725+JSN190@users.noreply.github.com>
2018-09-08 04:01:50 +0100

    Cascade delete rows from poll_options and poll_votes when poll deleted from polls

--- database.js
+++ database.js
@@ -15,18 +15,18 @@ async function initialise () {
             (id bigserial PRIMARY KEY, \
             name varchar(140) NOT NULL, \
             description varchar(500) NOT NULL, \
-            owner_id bigint REFERENCES users (id), \
+            owner_id bigint REFERENCES users (id) ON DELETE CASCADE, \
             created timestamp NOT NULL, \
-            modified timestamp NOT NULL)";
+            modified timestamp)";
         const createPollOptions = "CREATE TABLE IF NOT EXISTS poll_options \
             (id bigserial PRIMARY KEY, \
-            poll_id bigint REFERENCES polls (id) NOT NULL, \
+            poll_id bigint REFERENCES polls (id) ON DELETE CASCADE NOT NULL, \
             value varchar(140) NOT NULL, \
             created timestamp NOT NULL)";
         const createPollVotes = "CREATE TABLE IF NOT EXISTS poll_votes \
             (id bigserial PRIMARY KEY, \
             poll_id bigint REFERENCES polls (id) NOT NULL, \
-            poll_option_id bigint REFERENCES poll_options (id) NOT NULL, \
+            poll_option_id bigint REFERENCES poll_options (id) ON DELETE CASCADE NOT NULL, \
             created timestamp NOT NULL)";
         const setPollModified = "CREATE FUNCTION set_poll_modified() RETURNS \
             TRIGGER AS $$ BEGIN NEW.modified :=NOW(); RETURN NEW; END; $$ LANGUAGE plpgsql; \
@@ -36,6 +36,7 @@ async function initialise () {
         await client.query(createPolls);
         await client.query(createPollOptions);
         await client.query(createPollVotes);
+        //await client.query(setPollModified);
         await client.query("COMMIT");
         console.log(`Connected to PostgreSQL.`);
     } catch (e) {
